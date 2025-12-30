3e94ff0dc6543397aee8ad900f5277b03c576140
aegisfleet <aegisfleet@gmail.com>
2025-12-31 01:19:12 +0900

    feat: Add category filtering for business pricing and promotions in PAAPIClient

---
+++ src/api/__tests__/PAAPIClient.category.test.ts
@@ -0,0 +1,36 @@
+
+import { PAAPIClient } from '../PAAPIClient';
+
+describe('PAAPIClient Category Extraction', () => {
+    let client: PAAPIClient;
+
+    beforeEach(() => {
+        client = new PAAPIClient();
+    });
+
+    it('should filter out invalid category nodes including business pricing and promotions', () => {
+        const clientAny = client as any;
+
+        const invalidCategories = [
+            '【5％以上OFF】ホーム&キッチン用品 法人価格 | Amazonビジネス',
+            'AmazonGlobal',
+            'カテゴリー別',
+            '10% OFF',
+            'Prime Day セール'
+        ];
+
+        invalidCategories.forEach(cat => {
+            expect(clientAny.isValidCategoryNode(cat)).toBe(false);
+        });
+
+        const validCategories = [
+            'ホーム＆キッチン',
+            'キッチン用品',
+            'コーヒー・ティー用品'
+        ];
+
+        validCategories.forEach(cat => {
+            expect(clientAny.isValidCategoryNode(cat)).toBe(true);
+        });
+    });
+});
