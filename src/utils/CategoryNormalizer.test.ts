3874b2411c1bb7eda8493ecdcf458612baab8403
aegisfleet <aegisfleet@gmail.com>
2026-04-02 22:54:28 +0900

    feat: implement CategoryNormalizer utility with associated cache and test suite

--- src/utils/CategoryNormalizer.test.ts
+++ src/utils/CategoryNormalizer.test.ts
@@ -138,6 +138,7 @@ describe('CategoryNormalizer', () => {
         'その他のビジネス・経済関連書籍',
         'その他',
         '収納ラック　セット（ルミナス）',
+        '理美容以外の本体',
       ];
       invalidNames.forEach((name) => {
         expect(CategoryNormalizer.isValidCategoryName(name)).toBe(false);
